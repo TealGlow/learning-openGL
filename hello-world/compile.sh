b3c043cd7faa2c383a07b895769d641abe86f533
tealglow <alyssacomstock95@gmail.com>
2022-06-01 22:26:53 -0700

    added background color

--- hello-world/compile.sh
+++ hello-world/compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Bash script to compile and run just because I'm too lazy to have to keep hitting up :)
-#g++ -pthread -o helloworld helloworld.c glad.c -lglfw -lGLU -lGL
-#./helloworld
 gcc -pthread -o helloworld helloworld.c glad.c -lglfw -lGLU -lGL -lrt -ldl
 ./helloworld 
 
--- hello-world/helloworld.c
+++ hello-world/helloworld.c
@@ -1,3 +1,14 @@
+/* Name: Alyssa Comstock
+ * Language: C
+ * Description: Program that utilizes openGL, GLFW, and GLAD
+ * to create a window and render. This is done while learning
+ * openGL using the resources and textbook provided at 
+ * https://learnopengl.com/.  A majority of the code will
+ * be based on that but adapted for the C instead of C++
+ * */
+
+
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
@@ -5,24 +16,14 @@
 #include <string.h>
 
 
-//void framebuffer_size_callback(GLFWWindow* window, int width, int height);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-	glViewport(0,0,width,height);
-}
+// function prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void user_input(GLFWwindow * window);
 
-void user_input(GLFWwindow * window){
- 	// if the user hits the esc key the window will cloaw
-	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-		glfwSetWindowShouldClose(window, 1);
-	}
-
-}
 
 
 int main(){
-
-	
 	// init GLFW
 	glfwInit();
 
@@ -55,10 +56,13 @@ int main(){
 		// while the window is open
 		user_input(window);
 		// this is where rending happens in the loop
-
+		
+		// setting the background color to a nice teal color
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
 	}
 
 	// clean up
@@ -67,5 +71,36 @@ int main(){
 	return 0;
 }
 
+
+
+/* framebuffer_size_callback
+ * function that sets the opengl viewport if the window
+ * size changes
+ *
+ * @param: window: window struct object
+ * @param: width: width of the window
+ * @param: height: height of the window
+ * */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0, 0, width,height);
+}
+
+
+
+/* user_input
+ * function that handles when the user hits the enter key
+ * this is checked every single frame.  When the user
+ * hits escape the window is set to close.
+ *
+ * @param: window: window struct object
+ * */
+void user_input(GLFWwindow * window){
+ 	// if the user hits the esc key the window will cloaw
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+		// set should close to true
+		glfwSetWindowShouldClose(window, 1);
+	}
+
+}
 
 
